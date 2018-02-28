eebb833667e5b7687263f8bd205352fbf0bff7bc
Baruch <liliu888999@163.com>
2018-02-28 09:06:35 +0800

    Update SpringUtil.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/util/SpringUtil.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/util/SpringUtil.java
@@ -1,5 +1,33 @@
 package com.huawei.tdt.common.util;
 
-public class SpringUtil {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
+public class SpringUtil implements ApplicationContextAware {
+
+	private static ApplicationContext applicationContext = null;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		if (SpringUtil.applicationContext == null) {
+			SpringUtil.applicationContext = applicationContext;
+		}
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	public static Object getBean(String name) {
+		return getApplicationContext().getBean(name);
+	}
+
+	public static <T> T getBean(Class<T> clazz) {
+		return getApplicationContext().getBean(clazz);
+	}
+
+	public static <T> T getBean(String name, Class<T> clazz) {
+		return getApplicationContext().getBean(name, clazz);
+	}
 }
