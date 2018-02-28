4f63fd351010b9127566b1213488bf47d04269ad
Baruch <liliu888999@163.com>
2018-02-28 09:07:13 +0800

    Update UserVo.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/model/UserVo.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/model/UserVo.java
@@ -1,5 +1,22 @@
 package com.huawei.tdt.common.model;
 
-public class UserVo {
+import java.io.Serializable;
 
+public class UserVo implements Serializable{
+	private static final long serialVersionUID = -5050677211082623931L;
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
