83bbbdd764535acebfa9aeb2b54998c680a33d49
Baruch <liliu888999@163.com>
2018-02-28 09:12:48 +0800

    Update MvcConfig.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/authorization/config/MvcConfig.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/authorization/config/MvcConfig.java
@@ -1,5 +1,24 @@
 package com.huawei.tdt.common.authorization.config;
 
-public class MvcConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.huawei.tdt.common.authorization.intercepter.AuthorizationInterceptor;
+
+/**
+ * 配置类，增加自定义拦截器和解析器
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor);
+    }
 
 }
