02f4a5f0e91028fa9eb7d259c797feb42703fac0
Baruch <liliu888999@163.com>
2018-02-28 08:47:41 +0800

    Update TdtAuthenticateServiceApplication.java

--- td-server/tdt-authenticate-service/src/main/java/com/huawei/tdt/authenticate/TdtAuthenticateServiceApplication.java
+++ td-server/tdt-authenticate-service/src/main/java/com/huawei/tdt/authenticate/TdtAuthenticateServiceApplication.java
@@ -1,5 +1,22 @@
 package com.huawei.tdt.authenticate;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import com.huawei.tdt.common.util.SpringUtil;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = {"com.huawei.tdt"})
+@ComponentScan(basePackages = {"com.huawei.tdt"}) // Fix the issue that can not find the component defined in the other jars.
+@Import(value={SpringUtil.class})
 public class TdtAuthenticateServiceApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(TdtAuthenticateServiceApplication.class, args);
+	}
 }
