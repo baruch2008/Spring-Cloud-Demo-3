9b49f2dd2b67d2bde5ddb09db36e5e5f93d49ea0
Baruch <liliu888999@163.com>
2018-02-28 09:08:41 +0800

    Update DataSourceConfig.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/config/DataSourceConfig.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/config/DataSourceConfig.java
@@ -1,5 +1,27 @@
 package com.huawei.tdt.common.config;
 
-public class DataSourceConfig {
+import javax.sql.DataSource;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourceConfig {
+	@Bean(name = "dataSource")
+	@Primary
+	@ConfigurationProperties(prefix = "dbcp")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().type(org.apache.commons.dbcp2.BasicDataSource.class).build();
+	}
+
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory() {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource());
+		return bean;
+	}
 }
