74551d9baeb5cd6fade9344db9c2b5c5c6e3ee51
Baruch <liliu888999@163.com>
2018-02-28 08:57:42 +0800

    Update UserParams.java

--- td-server/tdt-gateway-service/src/main/java/com/huawei/tdt/gateway/model/UserParams.java
+++ td-server/tdt-gateway-service/src/main/java/com/huawei/tdt/gateway/model/UserParams.java
@@ -1,5 +1,22 @@
 package com.huawei.tdt.gateway.model;
 
-public class UserParams {
+import java.io.Serializable;
 
+public class UserParams implements Serializable{
+	private static final long serialVersionUID = -5050677211082623931L;
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
