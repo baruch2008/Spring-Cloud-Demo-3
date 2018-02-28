da0feaf71d4efe54cc71648a91673298f57e70d6
Baruch <liliu888999@163.com>
2018-02-28 09:06:56 +0800

    Update ResponseResult.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/model/ResponseResult.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/model/ResponseResult.java
@@ -1,5 +1,64 @@
 package com.huawei.tdt.common.model;
 
-public class ResponseResult {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.util.StringUtils;
+
+import com.huawei.tdt.common.authorization.constants.ResponseStatusEnum;
+
+public class ResponseResult implements Serializable {
+
+	private static final long serialVersionUID = -7769210729209279489L;
+
+	private ResponseStatusEnum status;
+
+	private String code;
+
+	private List<String> params = new ArrayList<String>();
+
+	private Object data;
+
+	public ResponseStatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(ResponseStatusEnum status) {
+		this.status = status;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public List<String> getParams() {
+		return params;
+	}
+
+	public void addParams(List<String> params) {
+		if (!StringUtils.isEmpty(params)) {
+			this.params.addAll(params);
+		}
+	}
+
+	public ResponseResult addParam(String param) {
+		if (null != param) {
+			this.params.add(param);
+		}
+
+		return this;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
