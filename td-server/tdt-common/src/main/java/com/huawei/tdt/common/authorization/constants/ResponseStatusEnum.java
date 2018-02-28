1f2c35f5af6f6a2f632506d4c09aceceace99578
Baruch <liliu888999@163.com>
2018-02-28 09:12:15 +0800

    Update ResponseStatusEnum.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/authorization/constants/ResponseStatusEnum.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/authorization/constants/ResponseStatusEnum.java
@@ -1,5 +1,19 @@
 package com.huawei.tdt.common.authorization.constants;
 
-public class ResponseStatusEnum {
+public enum ResponseStatusEnum {
+	FAIL(0), SUCCESS(1);
 
+	private int value;
+
+	private ResponseStatusEnum(int status) {
+		this.value = status;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public String toString() {
+		return String.valueOf(value);
+	}
 }
