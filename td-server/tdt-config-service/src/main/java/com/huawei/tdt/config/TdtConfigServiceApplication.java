863f9285c7c5c789ce182f72c92c561064a6f1af
Baruch <liliu888999@163.com>
2018-02-28 08:54:01 +0800

    Update TdtConfigServiceApplication.java

--- td-server/tdt-config-service/src/main/java/com/huawei/tdt/config/TdtConfigServiceApplication.java
+++ td-server/tdt-config-service/src/main/java/com/huawei/tdt/config/TdtConfigServiceApplication.java
@@ -1,5 +1,14 @@
 package com.huawei.tdt.config;
 
-public class TdtConfigServiceApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@SpringBootApplication
+@EnableConfigServer
+public class TdtConfigServiceApplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(TdtConfigServiceApplication.class, args);
+	}
 }
