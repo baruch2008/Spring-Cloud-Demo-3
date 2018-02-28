b7f183b822a6a86ebd0d55407556706c5ef94ef2
Baruch <liliu888999@163.com>
2018-02-28 08:56:00 +0800

    Update TdtEurekaServiceApplication.java

--- td-server/tdt-eureka-service/src/main/java/com/huawei/tdt/eureka/TdtEurekaServiceApplication.java
+++ td-server/tdt-eureka-service/src/main/java/com/huawei/tdt/eureka/TdtEurekaServiceApplication.java
@@ -1,5 +1,14 @@
 package com.huawei.tdt.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class TdtEurekaServiceApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(TdtEurekaServiceApplication.class, args);
+	}
 }
