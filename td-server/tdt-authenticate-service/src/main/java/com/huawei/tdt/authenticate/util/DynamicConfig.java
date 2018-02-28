f7c2080bee2df4fda7757ad3a3106b14c14b1abe
Baruch <liliu888999@163.com>
2018-02-28 08:49:20 +0800

    Update DynamicConfig.java

--- td-server/tdt-authenticate-service/src/main/java/com/huawei/tdt/authenticate/util/DynamicConfig.java
+++ td-server/tdt-authenticate-service/src/main/java/com/huawei/tdt/authenticate/util/DynamicConfig.java
@@ -1,5 +1,14 @@
 package com.huawei.tdt.authenticate.util;
 
-public class DynamicConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
+final class DynamicConfig {
+	@Value("${pITtoolCI.password}")
+	private String ldapAdminPassword;
+
+	public String getLdapAdminPassword() {
+		return ldapAdminPassword;
+	}
 }
