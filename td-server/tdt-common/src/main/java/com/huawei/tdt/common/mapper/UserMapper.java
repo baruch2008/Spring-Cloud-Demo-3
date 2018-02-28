100ccf765eb06a9488dbda7ff4e91a55559ff815
Baruch <liliu888999@163.com>
2018-02-28 09:07:35 +0800

    Update UserMapper.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/mapper/UserMapper.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/mapper/UserMapper.java
@@ -1,5 +1,14 @@
 package com.huawei.tdt.common.mapper;
 
-public class UserMapper {
+import org.springframework.stereotype.Repository;
 
+import com.huawei.tdt.common.entity.User;
+
+@Repository
+public interface UserMapper {
+	User getUserById(String id);
+	
+	void insertUser(User user);
+	
+	void updateUser(User user);
 }
