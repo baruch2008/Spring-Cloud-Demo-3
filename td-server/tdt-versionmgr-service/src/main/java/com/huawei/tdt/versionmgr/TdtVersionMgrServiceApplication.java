f28c103d5a67cdc0ea9382a70a236ea64ba88c9f
Baruch <liliu888999@163.com>
2018-03-01 08:53:43 +0800

    Update TdtVersionMgrServiceApplication.java

--- td-server/tdt-versionmgr-service/src/main/java/com/huawei/tdt/versionmgr/TdtVersionMgrServiceApplication.java
+++ td-server/tdt-versionmgr-service/src/main/java/com/huawei/tdt/versionmgr/TdtVersionMgrServiceApplication.java
@@ -1,5 +1,15 @@
 package com.huawei.tdt.versionmgr;
 
-public class TdtVersionMgrServiceApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.huawei.tdt"})
+public class TdtVersionMgrServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TdtVersionMgrServiceApplication.class, args);
+	}
 }
