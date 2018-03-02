eb46ed85e26678afc447f7f4e8c1237fc236ca28
Baruch <liliu888999@163.com>
2018-03-02 17:40:26 +0800

    Update Milestone.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/entity/Milestone.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/entity/Milestone.java
@@ -1,5 +1,65 @@
 package com.huawei.tdt.common.entity;
 
-public class Milestone {
+import java.io.Serializable;
 
+/**
+ * 项目里程碑
+ * 
+ * @author lWX537094
+ *
+ */
+public class Milestone implements Serializable {
+    private static final long serialVersionUID = -1808589303203673054L;
+
+    /**
+     * 里程碑ID，对应DB中的ID
+     */
+    private String id;
+
+    /**
+     * 里程碑名称
+     */
+    private String name;
+
+    /**
+     * 里程碑时间点
+     */
+    private long time;
+
+    /**
+     * 项目ID，对应DB中的ID
+     */
+    private String projectId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
 }
