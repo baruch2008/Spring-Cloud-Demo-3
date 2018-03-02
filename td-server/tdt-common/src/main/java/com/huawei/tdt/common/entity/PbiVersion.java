27b0c4c6af871237de180525b7eb869762f0a647
Baruch <liliu888999@163.com>
2018-03-02 17:40:45 +0800

    Update PbiVersion.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/entity/PbiVersion.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/entity/PbiVersion.java
@@ -1,5 +1,76 @@
 package com.huawei.tdt.common.entity;
 
-public class PbiVersion {
+import java.io.Serializable;
 
+/**
+ * PBI
+ * 
+ * @author lWX537094
+ *
+ */
+public class PbiVersion implements Serializable {
+    private static final long serialVersionUID = 3481538248497465979L;
+
+    /**
+     * PBI的ID
+     */
+    private String id;
+
+    private String category;
+
+    private String typeName;
+
+    private String cn;
+
+    private String en;
+
+    private String parentId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getCn() {
+        return cn;
+    }
+
+    public void setCn(String cn) {
+        this.cn = cn;
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public void setEn(String en) {
+        this.en = en;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
 }
