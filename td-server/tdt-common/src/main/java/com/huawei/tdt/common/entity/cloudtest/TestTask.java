a0ca57ba8fb029e1b1b8a61a26b9aac245364b14
Baruch <liliu888999@163.com>
2018-03-02 17:39:45 +0800

    Create TestTask.java

---
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/entity/cloudtest/TestTask.java
@@ -0,0 +1,59 @@
+package com.huawei.tdt.common.entity.cloudtest;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class TestTask implements Serializable {
+
+    private static final long serialVersionUID = -5073710137912480391L;
+
+    private String uri;
+
+    private String name;
+
+    private String stage;
+
+    private String taskType;
+
+    private List<TestCase> testCases;
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public List<TestCase> getTestCases() {
+        return testCases;
+    }
+
+    public void setTestCases(List<TestCase> testCases) {
+        this.testCases = testCases;
+    }
+}
