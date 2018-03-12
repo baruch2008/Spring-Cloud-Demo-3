520e503dc059e8d028383d4940122ea7c4da0b88
Baruch <liliu888999@163.com>
2018-03-12 09:04:50 +0800

    Create ProjectSynchronizeManager.java

---
+++ td-server/tdt-synchronize-service/src/main/java/com/huawei/tdt/synchronize/thread/ProjectSynchronizeManager.java
@@ -0,0 +1,18 @@
+package com.huawei.tdt.synchronize.thread;
+
+/**
+ * 用于初始化项目数据同步任务
+ * 
+ * @author lWX537094
+ */
+public class ProjectSynchronizeManager implements Runnable
+{
+    //private ProjectMapper projectMapper = SpringUtil.getBean(ProjectMapper.class);
+
+    @Override
+    public void run()
+    {
+        //List<Project> projects = projectMapper.getAllProjects();
+        //System.out.println(projects.size());
+    }
+}
