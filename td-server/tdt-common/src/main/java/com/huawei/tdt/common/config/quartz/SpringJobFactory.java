7aecf6ebe3a11cf1b4228a8945f75efdfa7db106
Baruch <liliu888999@163.com>
2018-03-28 13:46:00 +0800

    Create SpringJobFactory.java

---
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/config/quartz/SpringJobFactory.java
@@ -0,0 +1,27 @@
+package com.huawei.tdt.common.config.quartz;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.AdaptableJobFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringJobFactory
+ * 
+ * @author lWX537094
+ */
+@Component
+public class SpringJobFactory extends AdaptableJobFactory
+{
+    @Autowired
+    private AutowireCapableBeanFactory capableBeanFactory;
+
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception
+    {
+        Object jobInstance = super.createJobInstance(bundle);
+        capableBeanFactory.autowireBean(jobInstance);
+        return jobInstance;
+    }
+}
