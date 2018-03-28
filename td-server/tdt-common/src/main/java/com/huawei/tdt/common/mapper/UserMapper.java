835030c3c27c57d241242898299302e92b34febe
Baruch <liliu888999@163.com>
2018-03-28 13:49:07 +0800

    Update UserMapper.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/mapper/UserMapper.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/mapper/UserMapper.java
@@ -1,14 +1,51 @@
 package com.huawei.tdt.common.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.huawei.tdt.common.entity.User;
 
+/**
+ * 用户映射类.
+ */
 @Repository
-public interface UserMapper {
-	User getUserById(String id);
-	
-	void insertUser(User user);
-	
-	void updateUser(User user);
+public interface UserMapper
+{
+    /**
+     * 用id获取用户.
+     *
+     * @param id 用户id
+     * @return 用户对象
+     */
+    User getUserById(String id);
+
+    /**
+     * 插入用户.
+     *
+     * @param user 用户对象
+     */
+    void insertUser(User user);
+    
+    /**
+     * 保存或更新用户
+     * @param user 用户
+     */
+    void saveOrUpdate(User user);
+
+    /**
+     * 更新用户.
+     *
+     * @param user 用户对象
+     */
+    void updateUser(User user);
+
+    /**
+     * 查询用户在指定项目中的权限
+     * 
+     * @param projectId 项目ID
+     * @param userId 用户ID
+     * @return 权限集
+     */
+    List<String> getUserPrivileges(String projectId, String userId);
 }
