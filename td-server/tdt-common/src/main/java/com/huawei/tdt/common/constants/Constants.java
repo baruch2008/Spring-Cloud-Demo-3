1d11765eac9ad16828ba828e09a94d827138f5b6
Baruch <liliu888999@163.com>
2018-03-28 13:48:08 +0800

    Update Constants.java

--- td-server/tdt-common/src/main/java/com/huawei/tdt/common/constants/Constants.java
+++ td-server/tdt-common/src/main/java/com/huawei/tdt/common/constants/Constants.java
@@ -1,15 +1,158 @@
 package com.huawei.tdt.common.constants;
 
-public final class Constants {
-    private Constants() {}
+/**
+ * 常量类
+ * 
+ * @author lWX537094
+ */
+public final class Constants
+{
+    /**
+     * JOB_GROUP
+     */
+    public static final String JOB_GROUP = "tdt_job_group";
 
+    /**
+     * TRIGGER_GROUP
+     */
+    public static final String TRIGGER_GROUP = "tdt_trigger_group";
+
+    /**
+     * ZERO
+     */
+    public static final int ZERO = 0;
+
+    /**
+     * ONE
+     */
     public static final int ONE = 1;
 
+    /**
+     * TWO
+     */
+    public static final int TWO = 2;
+
+    /**
+     * THREE
+     */
+    public static final int THREE = 3;
+
+    /**
+     * TWO
+     */
+    public static final int FOUR = 4;
+
+    /**
+     * FIVE
+     */
+    public static final int FIVE = 5;
+
+    /**
+     * SIX
+     */
+    public static final int SIX = 6;
+
+    /**
+     * SEVEN
+     */
+    public static final int SEVEN = 7;
+
+    /**
+     * EIGHT
+     */
+    public static final int EIGHT = 8;
+
+    /**
+     * NINE
+     */
+    public static final int NINE = 9;
+
+    /**
+     * TEN
+     */
     public static final int TEN = 10;
-
+    
+    /**
+     * ELEVEN
+     */
+    public static final int ELEVEN = 11;
+    
+    /**
+     * TWELVE
+     */
     public static final int TWELVE = 12;
+    
+    /**
+     * THIRTEEN
+     */
+    public static final int THIRTEEN = 13;
+    
+    /**
+     * FOURTEEN
+     */
+    public static final int FOURTEEN = 14;
+    
+    /**
+     * FIFTEEN
+     */
+    public static final int FIFTEEN = 15;
+    
+    /**
+     * SIXTEEN
+     */
+    public static final int SIXTEEN = 16;
+    /**
+     * 规避“魔数”.
+     */
+    public static final int HTTP_OK = 200;
 
-    public static final String TRIGGER_GROUP = "TDT_GRIGGER_GROUP";
+    /**
+     * 资源查看权限ID
+     */
+    public static final String TDT_PRIVILEGE_RES_VIEW = "TDT_PRIVILEGE_RES_VIEW";
 
-    public static final String JOB_GROUP = "TDT_JOB_GROUP";
+    /**
+     * 资源创建权限ID
+     */
+    public static final String TDT_PRIVILEGE_RES_CREATE = "TDT_PRIVILEGE_RES_CREATE";
+
+    /**
+     * 资源修改权限ID
+     */
+    public static final String TDT_PRIVILEGE_RES_MODIFY = "TDT_PRIVILEGE_RES_MODIFY";
+
+    /**
+     * 资源删除权限ID
+     */
+    public static final String TDT_PRIVILEGE_RES_DELETE = "TDT_PRIVILEGE_RES_DELETE";
+
+    /**
+     * 资源人员配置权限ID
+     */
+    public static final String TDT_PRIVILEGE_MEM_CONFIG = "TDT_PRIVILEGE_MEM_CONFIG";
+
+    /**
+     * 资源项目计划管理权限ID
+     */
+    public static final String TDT_PRIVILEGE_PRJ_PLAN_MGR = "TDT_PRIVILEGE_PRJ_PLAN_MGR";
+
+    /**
+     * 存储当前登录用户id的字段名.
+     */
+    public static final String CURRENT_USER_ID = "CURRENT_USER_ID";
+
+    /**
+     * 存放Authorization的header字段.
+     */
+    public static final String AUTHORIZATION = "authorization";
+
+    /**
+     * 资源ID
+     */
+    public static final String RESOURCE_ID = "resourceId";
+
+    private Constants()
+    {
+
+    }
 }
