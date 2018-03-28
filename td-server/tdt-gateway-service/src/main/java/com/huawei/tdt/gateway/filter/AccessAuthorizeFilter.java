5562462a777e3042fc746dc2c87e1683d2028643
Baruch <liliu888999@163.com>
2018-03-28 14:14:21 +0800

    Update AccessAuthorizeFilter.java

--- td-server/tdt-gateway-service/src/main/java/com/huawei/tdt/gateway/filter/AccessAuthorizeFilter.java
+++ td-server/tdt-gateway-service/src/main/java/com/huawei/tdt/gateway/filter/AccessAuthorizeFilter.java
@@ -2,6 +2,8 @@ package com.huawei.tdt.gateway.filter;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.ribbon.apache.RibbonApacheHttpResponse;
 
@@ -9,62 +11,93 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
 /**
- * 访问授权过滤器，处理用户登录
- * 
- * @author lWX537094
+ * 访问授权过滤器，处理用户登录.
  *
+ * @author lWX537094
  */
-public class AccessAuthorizeFilter extends ZuulFilter {
-	@Value("${app.product.cookiename:DEFAULTSESSION}")
-	private String cookieName;
+public class AccessAuthorizeFilter extends ZuulFilter
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessAuthorizeFilter.class);
 
-	@Value("${app.product.session.timeout:30}")
-	private int sessionTimeoutInMinuts;
+    @Value("${tdt.isNeedAuthFilter}")
+    private boolean isNeedAuthFilter = true;
 
-	public boolean shouldFilter() {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		String path = ctx.getRequest().getRequestURI();
+    /**
+     * 是否应该对当前上下文中的请求做过滤.
+     *
+     * @return true：过滤；false：不过滤
+     */
+    public boolean shouldFilter()
+    {
+        if (!isNeedAuthFilter)
+        {
+            return false;
+        }
 
-		// 请求路径为/auth/login则处理，其它的不处理
-		if (null != path && (path.startsWith("/auth/login") || path.startsWith("project/login"))) {
-			return true;
-		}
+        RequestContext ctx = RequestContext.getCurrentContext();
+        String path = ctx.getRequest().getRequestURI();
 
-		return false;
-	}
+        // 请求路径为/auth/login则处理，其它的不处理
+        if (null != path && path.startsWith("/auth/login"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-	public Object run() {
+    /**
+     * 执行过滤.
+     *
+     * @return 过滤的执行结果
+     */
+    public Object run()
+    {
 
-		RequestContext ctx = RequestContext.getCurrentContext();
+        RequestContext ctx = RequestContext.getCurrentContext();
 
-		if (isSuccessLogin(ctx)) {
-			// TODO
-		}
+        if (isSuccessLogin(ctx))
+        {
+            LOGGER.info("Login successed.");
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public String filterType() {
-		return "post";
-	}
+    @Override
+    public String filterType()
+    {
+        return "post";
+    }
 
-	@Override
-	public int filterOrder() {
-		return 1;
-	}
+    @Override
+    public int filterOrder()
+    {
+        return 1;
+    }
 
-	private boolean isSuccessLogin(RequestContext ctx) {
-		RibbonApacheHttpResponse ribbonResp = (RibbonApacheHttpResponse) ctx.get("ribbonResponse");
-		String isSuccess = ribbonResp.getHttpHeaders().getFirstValue("isSuccess");
-		if (HttpServletResponse.SC_OK != ribbonResp.getStatus()) {
-			return false;
-		}
+    /**
+     * 是否登录成功.
+     *
+     * @param ctx 当前上下文
+     * @return true：成功；false：失败
+     */
+    private boolean isSuccessLogin(final RequestContext ctx)
+    {
+        RibbonApacheHttpResponse ribbonResp = (RibbonApacheHttpResponse) ctx.get("ribbonResponse");
+        String isSuccess = ribbonResp.getHttpHeaders().getFirstValue("isSuccess");
+        if (HttpServletResponse.SC_OK != ribbonResp.getStatus())
+        {
+            return false;
+        }
 
-		if ("true".equals(isSuccess)) {
-			return true;
-		}
+        if ("true".equals(isSuccess))
+        {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
