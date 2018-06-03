550b9deab147b6fa6aa03d99ec74f96a3a749537
baruch2008 <baruch2008@126.com>
2018-06-03 20:32:43 +0800

    modify config

--- td-server/tdt-authenticate-service/docker/startService.sh
+++ td-server/tdt-authenticate-service/docker/startService.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /usr/local/tdt/authenticate/log
 
-/usr/local/jdk1.8.0_162/bin/java -jar /usr/local/tdt-authenticate-service-0.1.jar > /usr/local/tdt/authenticate/log/tdt-authenticate-service.log
+/usr/local/jdk1.8.0_162/bin/java -jar /usr/local/tdt-authenticate-service-0.1.jar --spring.profiles.active=pro > /usr/local/tdt/authenticate/log/tdt-authenticate-service.log
