3b1773903c7ae9e1edbd8abed8fceaf2697a5859
baruch2008 <baruch2008@126.com>
2018-06-03 19:57:28 +0800

    test

--- td-server/tdt-config-service/docker/startService.sh
+++ td-server/tdt-config-service/docker/startService.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /usr/local/tdt/config/log
 
-java -jar /usr/local/tdt-config-service-0.1.jar > /usr/local/tdt/config/log/tdt-config-service.log
+/usr/local/jdk1.8.0_162/bin/java -jar /usr/local/tdt-config-service-0.1.jar > /usr/local/tdt/config/log/tdt-config-service.log
--- td-server/tdt-eureka-service/docker/startService.sh
+++ td-server/tdt-eureka-service/docker/startService.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /usr/local/tdt/eureka/log
 
-java -jar /usr/local/tdt-eureka-service-0.1.jar > /usr/local/tdt/eureka/log/tdt-eureka-service.log
+/usr/local/jdk1.8.0_162/bin/java -jar /usr/local/tdt-eureka-service-0.1.jar > /usr/local/tdt/eureka/log/tdt-eureka-service.log
