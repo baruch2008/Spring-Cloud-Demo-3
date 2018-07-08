bf9072c438745dbf245716d719ddff4911618f2b
baruch2008 <baruch2008@126.com>
2018-07-08 08:46:15 +0800

    增加MongoDB操作DAO

---
+++ td-server/tdt-common/src/main/java/com/huawei/allinone/config/MongoDBConfig.java
@@ -0,0 +1,29 @@
+package com.huawei.allinone.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MongoDBConfig {
+    @Value("${mongo.db.name}")
+    private String dbName;
+
+    @Value("${mongo.db.host}")
+    private String host;
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
---
+++ td-server/tdt-common/src/main/java/com/huawei/allinone/dao/BaseMongoDBDao.java
@@ -0,0 +1,48 @@
+package com.huawei.allinone.dao;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.huawei.allinone.config.MongoDBConfig;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoException;
+
+public class BaseMongoDBDao {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseMongoDBDao.class);
+
+    @Autowired
+    private MongoDBConfig dbConfig;
+    
+    private MongoClient mongoClient = null;
+
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    protected MongoClient getMongoClient() {
+        lock.writeLock().lock();
+        try {
+            if (null == mongoClient) {
+                MongoClientOptions.Builder build = new MongoClientOptions.Builder();
+                build.connectionsPerHost(50);
+                build.threadsAllowedToBlockForConnectionMultiplier(50);
+
+                build.maxWaitTime(1000 * 60 * 2);
+                build.connectTimeout(1000 * 60 * 1);
+
+                MongoClientOptions myOptions = build.build();
+                try {
+                    mongoClient = new MongoClient(dbConfig.getHost(), myOptions);
+                } catch (MongoException e) {
+                    LOGGER.error("Getting mongo client failed.", e);
+                }
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
+
+        return mongoClient;
+    }
+}
